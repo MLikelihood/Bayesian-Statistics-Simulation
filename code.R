373196e96fdc0dafa760a7613d3d44fe4015ac21
Max Likelihood <shuzhang@iastate.edu>
2017-06-07 23:03:49 -0500

    Create code.R

--- code.R
+++ code.R
@@ -86,7 +86,51 @@ res = ddply(melt(sum, id.vars=c("X1","lambda","alpha","beta")),
 res
 
 
+#homework 4 -stan model
+d = data.frame(year=1976:1985,
+fatal_accidents = c(24,25,31,31,22,21,26,20,16,22),
+passenger_deaths = c(734,516,754,877,814,362,764,809,223,1066),
+death_rate = c(0.19,0.12,0.15,0.16,0.14,0.06,0.13,0.13,0.03,0.15))
+d$miles_flown = d$passenger_deaths/d$death_rate # 100 million miles
+d
 
 
+prior=list(a_mu = 1,
+b_mu = 5,
+a_beta = 6/10,
+b_beta = 1/10000)
 
-
+library(rstan)
+stan.model = "
+data f
+int<lower=0> N; //number of observations
+int y[N]; //number of fatal crashes
+real x[N]; //the distance traveled
+real<lower=0> a_mu; //fixed elements in priors
+real<lower=0> b_mu;
+real<lower=0> a_beta;
+real<lower=0> b_beta;
+g
+parameters f
+real<lower=0> lambda[N]; //parameters with priors
+real<lower=0> mu;
+real<lower=0> beta;
+g
+transformed parameters f
+real<lower=0> alpha; //functions of parameters with priors
+real<lower=0> sigma;
+alpha <- mu*beta;
+sigma <- sqrt(alpha)/beta;
+g
+model f
+for(i in 1:N)f
+y[i] ~ poisson(x[i]*lambda[i]);
+lambda[i] ~ gamma(alpha,beta);
+g
+mu ~ gamma(a_mu, b_mu);
+beta ~ gamma(a_beta,b_beta);
+g "
+dat = list(N=nrow(d), y=d$fatal_accidents, x=d$miles_flown,
+a_mu = 1, b_mu = 5,
+a_beta = 6/10, b_beta = 1/10000)
+fit = stan(model_code = stan.model, data = dat, iter = 10000, chains=3)
